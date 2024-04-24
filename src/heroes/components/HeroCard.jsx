a8464cbd14255837e47a5daecbdc1ee8e17f2d9c
marco-soria <marco.soria@pucp.edu.pe>
2024-04-23 21:21:11 -0500

    herocard

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+const CharactersByHero = ({alter_ego, characters}) => {
+    /* if (alter_ego === characters) return (<></>);
+    
+    return <p>{characters}</p> */
+    return (alter_ego !== characters) ? <p>{characters}</p> : <></>
+}
 
 export const HeroCard = ({
     id,
@@ -8,7 +16,7 @@ export const HeroCard = ({
     first_appearance,
     characters,
 }) => {
-    const heroImageUrl = `../../assets/heroes/${id}.jpg`
+    const heroImageUrl = `../src/assets/heroes/${id}.jpg`
     console.log(heroImageUrl)
 
   return (
@@ -21,6 +29,24 @@ export const HeroCard = ({
                             <img src={heroImageUrl} alt={superhero} className='card-img' />
 
                         </div>
+                    </div>
+                </div>
+                <div className='col-8'>
+                    <div className='card-body'>
+                        <h5 className='card-title'>{superhero}</h5>
+                        <p className='card-text'>{alter_ego}</p>
+                        {/* {
+                            (alter_ego !== characters) && <p className='card-text'>{characters}</p>
+                        } */}
+
+                        <CharactersByHero alter_ego={alter_ego} characters={characters} />
+                        <p className='card-text'>
+                            <small className='text-muted'>{first_appearance}</small>
+                        </p>
+
+                        <Link to={`./hero/${id}`}>
+                            Más...
+                        </Link>
                     </div>
                 </div>
                 
